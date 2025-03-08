b1c893bb44d95b45892120400da931441249ed4b
ImanM9 <imanmotlagh17@gmail.com>
2025-03-07 19:39:52 -0500

    simple setups

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold">
+        Welcome to <span className="text-gray-400">MochiNut</span>
+      </h1>
+      <p className="text-2xl font-medium text-gray-400">
+        Mochi is a web3 social media platform that allows users to create and
+        share content, connect with friends, and discover new communities.
+      </p>
+    </div>
+  );
+}
